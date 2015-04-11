50e1d90ae9a6682d5e3c3c92ad87c86032d40913
RuiChong <739858341@qq.com>
2015-04-11 22:00:30 +0800

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-#coding=utf-8
-#from mycore import MyScanner
-import os,sys,re,requests,socket
-d = "username=chongrui&pass=1123&cookie=xxxxssss"
-print dict([tuple(x.split("=")) for x in d.split("&")])
-# data = dict([(k,v) for ])
-
-
